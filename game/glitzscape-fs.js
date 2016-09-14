eda90747570a4e1d07744f0d887dbad9be79a3c3
alinen <nenila@gmail.com>
2016-09-14 16:32:17 -0400

    animate background

--- game/glitzscape-fs.js
+++ game/glitzscape-fs.js
@@ -3,6 +3,7 @@ var fragmentShaderSource_Tex = `
    varying vec2 vTextureCoord;
    varying vec4 vColor;
    uniform sampler2D uSampler;
+   uniform float uT;
 
    void main(void) 
    {
@@ -11,9 +12,51 @@ var fragmentShaderSource_Tex = `
    }
 `;
 
+var fragmentShaderSource_Noise = `
+   precision mediump float;
+   varying vec2 vTextureCoord;
+   varying vec4 vColor;
+   uniform sampler2D uSampler;
+   uniform float uT;
+
+   void main(void) 
+   {
+      vec4 textureColor = texture2D(uSampler, vec2(vTextureCoord.s+uT,vTextureCoord.t));
+      gl_FragColor = vec4(textureColor.rgb * vColor.rgb, textureColor.a * vColor.a);
+   }
+`;
+
+
+var fragmentShaderSource_NoiseSpin = `
+   precision mediump float;
+   varying vec2 vTextureCoord;
+   varying vec4 vColor;
+   uniform sampler2D uSampler;
+   uniform float uT;
+
+   void main(void) 
+   {
+      float s = (vTextureCoord.s-0.5)*2.0; // center around (0,0)
+      float t = (vTextureCoord.t-0.5)*2.0;
+
+      float r = length(vec2(s,t)); // rotate
+      s = r*sin(uT);
+      t = r*cos(uT);
+
+      s = s*0.5+0.5; // move back
+      t = t*0.5+0.5; // move back
+      vec4 textureColor = texture2D(uSampler, vec2(s, t));
+      gl_FragColor = vec4(textureColor.rgb * vColor.rgb, textureColor.a * vColor.a);
+   }
+`;
+
+
 var fragmentShaderSource_Solid = `
    precision mediump float;
+   varying vec2 vTextureCoord;
    varying vec4 vColor;
+   uniform sampler2D uSampler;
+   uniform float uT;
 
    void main(void) 
    {
@@ -27,6 +70,7 @@ var fragmentShaderSource_TexInvert = `
    varying vec2 vTextureCoord;
    varying vec4 vColor;
    uniform sampler2D uSampler;
+   uniform float uT;
 
     void main(void) 
     {
